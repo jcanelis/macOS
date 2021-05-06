77017a3bbe409569a4e3dffb005b2252d5c90a15
John Canelis <jcanelis@gmail.com>
2021-05-06 00:56:35 -0400

    Initial Commit

---
+++ ReturnOfTheMac/ReturnOfTheMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReturnOfTheMacApp.swift
+//  ReturnOfTheMac
+//
+//  Created by John Canelis on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ReturnOfTheMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
